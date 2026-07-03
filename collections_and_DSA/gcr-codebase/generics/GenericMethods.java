c7363c05b951749e92dbe538d6390628d2b68300
Tanisha Gupta <tanishag1906@gmail.com>
2026-07-03 22:12:07 +0530

    [Tanisha Gupta] Add: Added generics related problems

---
+++ collections_and_DSA/gcr-codebase/generics/GenericMethods.java
@@ -0,0 +1,13 @@
+gclass Utility {
+    public static <T> void printDetails(T data) {
+        System.out.println("Details: " + data);
+    }
+}
+
+public class GenericMethods {
+    public static void main(String[] args) {
+        Utility.printDetails("Flight AA202");
+        Utility.printDetails(101);
+        Utility.printDetails(99.5);
+    }
+}
